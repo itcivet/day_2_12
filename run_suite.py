238e0ba615533fa7243dac7753443aa795485072
yyyyy <yyy@163>
2020-02-14 18:09:37 +0800

    修改一点提示信息

--- run_suite.py
+++ run_suite.py
@@ -2,10 +2,10 @@
 import unittest
 from HTMLTestRunner import HTMLTestRunner
 
-# 2组装测试套件
+# 2组装测试 套件
 suite = unittest.defaultTestLoader.discover("./scripts", pattern="test*.py")
 
-# 3.获取报告存储文件流 实例化htmltestrunner 调用run执行测试套件
+# 3.获取报告存  储文件流 实例化htmltestrunner 调用run执行测试套件
 
 with open("./report/ihrm_report.html", "wb")as f:
     HTMLTestRunner(stream=f).run(suite)
